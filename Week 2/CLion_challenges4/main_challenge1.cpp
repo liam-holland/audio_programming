2aa89d9b55fac51a631bacde36f216416b941040
liam-holland <liam.holland96@gmail.com>
2026-01-27 17:42:06 +0000

    Challenge 1

--- Week 2/CLion_challenges4/main_challenge1.cpp
+++ Week 2/CLion_challenges4/main_challenge1.cpp
@@ -14,6 +14,7 @@
 
 */
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <cmath>                // for sin and M_PI
@@ -21,18 +22,34 @@
 
 int main(int argc, const char * argv[]) {
 
-    int sampleRate = 44100;
+    const int sampleRate = 44100;
 
-    int duration = 10;                               // duration in seconds
-    int durationInSamples = sampleRate * duration;  // total number of samples for the whole output
+    const int duration = 10;                              // duration in seconds
+    const int durationInSamples = sampleRate * duration;  // total number of samples for the whole output
 
     std::vector<float> samples (durationInSamples);  // create a vector to store all of our samples
 
     float volume{0.1};
-    float frequency = 660;                          // the frequency of the tone to generate
+    const float initialFrequency = 660;                          // the frequency of the tone to generate
+    float frequency{initialFrequency};
 
-    float phase = 0.0;                                  // keep track of the phase of our oscillator
-    float phaseDelta = frequency  / float(sampleRate);  // phaseDelta - how much increment the phase by for each sample
+    const float initialFrequency2 = 440;
+    float frequency2{initialFrequency2};
+
+    float phase{0};
+    float phase2{0};
+
+    float phaseDelta{};
+    float phaseDelta2{};
+
+    // Set up variables that will count how many samples until we half the frequency
+    // bool flip{true};
+    int samplesUntilSwitch{0};
+
+
+    float flipTime{5.0};
+    int sampleFlip{  static_cast<int>(flipTime * sampleRate) };
+    int flipCounter{0};
 
 
     // ************
@@ -40,20 +57,40 @@ int main(int argc, const char * argv[]) {
     for (int i=0; i<durationInSamples; i++)
     {
 
+        samplesUntilSwitch = sampleFlip - i;
+
+        if (samplesUntilSwitch < 0) {
+            frequency = initialFrequency / 2;
+
+            flipCounter++;
+
+            samplesUntilSwitch = sampleFlip*(flipCounter+1);
+        }
+
         // calculate the sine tone value from this phase --> with sin(phase * 2 * pi)
         float sample = sin( phase * 2 * M_PI );
+        float sample2 = sin( phase2 * 2 * M_PI );
 
         // add this sample to our samples vector (also halving the volume)
-        samples[i] = volume * sample;
+        samples[i] = volume * sample + volume * sample2;
 
+        phaseDelta = frequency  / float(sampleRate);  // phaseDelta - how much increment the phase by for each sample
+        phaseDelta2 = frequency2 / float(sampleRate);
 
         // increment the phase a little
         phase += phaseDelta;
+        phase2 += phaseDelta2;
 
         // make sure the phase is in the range 0-1
         if (phase > 1.0)
         {
             phase -= 1.0;
+        }
+
+        // make sure the phase is in the range 0-1
+        if (phase2 > 1.0)
+        {
+            phase2 -= 1.0;
         }
 
     }
